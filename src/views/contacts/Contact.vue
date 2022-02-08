17711a2fb26359aa43e878b0ffecd3e54aefd3d7
puldyastin <puldyastin@gmail.com>
2022-02-08 11:10:39 +0800

    dynamic router

--- src/views/contacts/Contact.vue
+++ src/views/contacts/Contact.vue
@@ -2,7 +2,9 @@
   <div class="about">
     <h1>This is an contact page list</h1>
     <div v-for="contact in contacts" :key="contact.id">
-      <p>{{ contact.gmail }}</p>
+      <router-link :to="{ name: 'ContactDetails', params: { id: contact.id } }">
+        <p>{{ contact.title }}</p>
+      </router-link>
     </div>
   </div>
 </template>
@@ -12,9 +14,14 @@ export default {
   data() {
     return {
       contacts: [
-        { id: 1, gmail: "contact1@gmail", fb: "dsadsa.fb" },
-        { id: 2, gmail: "contact2@gmail", fb: "sdfdsf.fb" },
-        { id: 3, gmail: "contact3@gmail", fb: "fdsfdsfs.fb" },
+        { id: 1, title: "Contact1", gmail: "contact1@gmail", fb: "dsadsa.fb" },
+        { id: 2, title: "Contact2", gmail: "contact2@gmail", fb: "sdfdsf.fb" },
+        {
+          id: 3,
+          title: "Contact3",
+          gmail: "contact3@gmail",
+          fb: "fdsfdsfs.fb",
+        },
       ],
     };
   },
--- src/views/contacts/ContactDetails.vue
+++ src/views/contacts/ContactDetails.vue
@@ -1,3 +1,14 @@
 <template>
   <div>Contact details</div>
+  <p>the contact id is {{ id }}</p>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+};
+</script>
