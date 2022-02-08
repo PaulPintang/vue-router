bc5bccafd2d885e6626f4dc982088d266b6c074a
puldyastin <puldyastin@gmail.com>
2022-02-08 10:33:44 +0800

    add binding in router links instead of using path

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/contact">Contact</router-link>
+    <router-link :to="{ name: 'About' }">About</router-link> |
+    <router-link :to="{ name: 'Contact' }">Contact</router-link>
   </div>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Contact from "../views/Contact.vue";
 
 const routes = [
   {
@@ -13,8 +15,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
   {
     path: "/contact",
@@ -22,8 +25,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
+    component: Contact,
   },
 ];
 
